87da32ad00f9648ce37afcded6c723070ea49f49
Ming <714486244@qq.com>
2019-11-04 03:15:01 +0800

    111
    111

---
+++ seleniumDemo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+BASE_URL = 'https://appointment.bmeia.gv.at/'
+
+
+class Browser(object):
+    def __init__(self):
+        pass
+
+
+# -----------------------------------------------------------------------------
+if __name__ == '__main__':
+    pass
--- task.py
+++ task.py
@@ -16,8 +16,14 @@ import numpy as np
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 from aip import AipOcr
 
+# from PyQt5.QtCore import QThread
+
+requests.packages.urllib3.disable_warnings()
 BASE_URL = 'https://appointment.bmeia.gv.at/'
 SERV_URL = 'http://localhost:5555/'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10',
+}
 TOKEN_PATTERN = re.compile(r'<input id="Token" name="Token" type="hidden" value="(.*?)"')
 BAIDU_OCR_OPTIONS = {}
 BAIDU_OCR_OPTIONS["recognize_granularity"] = "small"
@@ -35,8 +41,9 @@ def fetchClients():
     """
     从服务器获取客户信息,并分类(北京、上海、北京调试、上海调试)
     """
-    global G_DEBUG, G_PEKING_CLIENTS, G_SHANGHAI_CLIENTS
-    r = requests.get(f'{SERV_URL}clients')
+    global G_DEBUG, G_PEKING_CLIENTS, G_SHANGHAI_CLIENTS, G_PEKING_CLIENTS_DEBUG, G_SHANGHAI_CLIENTS_DEBUG
+    """
+    r = requests.get(f'{SERV_URL}clients', headers=HEADERS)
     if r.status_code == 200:
         res = r.json()
         if res['status'] == 'ok':
@@ -56,6 +63,7 @@ def fetchClients():
                     G_SHANGHAI_CLIENTS_DEBUG.append(client)
                 else:
                     G_SHANGHAI_CLIENTS.append(client)
+            # TODO: 其他Office就在这里添加相应的!!
             # print('G_PEKING_CLIENTS: ', G_PEKING_CLIENTS)
             # print('G_SHANGHAI_CLIENTS: ', G_SHANGHAI_CLIENTS)
             # print('G_PEKING_CLIENTS_DEBUG: ', G_PEKING_CLIENTS_DEBUG)
@@ -68,6 +76,31 @@ def fetchClients():
             print('err')
     else:
         print('服务器访问失败')
+    """
+    G_SHANGHAI_CLIENTS.append({
+        'CalendarType' : 'SHANGHAI',
+        'StartTime' : '2/1/2020',
+        'Firstname' : 'JIAN',
+        'Lastname' : 'YAO',
+        'DateOfBirth' : '17.06.1975',
+        'TraveldocumentNumber' : 'E70681011',
+        'Sex' : '1',
+        'Street' : '1104building9,xidi international district',
+        'Postcode' : '210019',
+        'City' : 'Nanjing',
+        'Country' : '46',
+        'Telephone' : '18702194830',
+        'Email' : 'harry_humail@hotmail.com',
+        'LastnameAtBirth' : 'YAO',
+        'NationalityAtBirth' : '49',
+        'CountryOfBirth' : '49',
+        'PlaceOfBirth' : 'JIANGSU',
+        'NationalityForApplication' : '49',
+        'TraveldocumentDateOfIssue' : '23.06.2016',
+        'TraveldocumentValidUntil' : '22.06.2026',
+        'TraveldocumentIssuingAuthority' : '49',
+    })
+    return True
 
 
 def setBaiduOCRClients(filepath):
@@ -95,7 +128,7 @@ def setBaiduOCRClients(filepath):
 
 
 def fetchBaiduOCRClients(start=0, end=5):
-    r = requests.get(f'{SERV_URL}baiduClients/{start}/{end}')
+    r = requests.get(f'{SERV_URL}baiduClients/{start}/{end}', headers=HEADERS)
     if r.status_code == 200:
         clients = r.json()['data']
         global G_Q_OCR
@@ -106,7 +139,7 @@ def fetchBaiduOCRClients(start=0, end=5):
         return True
 
 
-def loopFetchAndSetShanghaiCalendarId():
+def loopFetchAndSetUnknownCalendarId(office):
     """
     正式上线时调用!用户不断地获取上海CalendarId,直至获取成功后才开始抢上海部分
     :return:
@@ -114,11 +147,18 @@ def loopFetchAndSetShanghaiCalendarId():
     # TIPS: 上线时记得换成 Niederlassung
     keyword = 'Niederlassung'  # 到时候要抓取的关键字
     # keyword = '长达6个月'  # 到时候要抓取的关键字
-    url = f'{BASE_URL}?office=shanghai'
+    url = f'{BASE_URL}?office={office}'
+    playload = {
+        'Language': 'zh',
+        'Command': '变更',
+        'Office': office,
+        'CalendarId': '0',
+    }
     pattern = re.compile(r'<option value="(.*?)">.*?%s.*?</option>' % keyword.replace('"', '&quot;'))
     while True:
         try:
-            r = requests.get(url)
+            print('开始抓取上海的CalendarId')
+            r = requests.post(url, data=playload, headers=HEADERS)
             if r.status_code == 200:
                 text = r.text
                 searchResult = re.search(pattern, text)
@@ -135,19 +175,16 @@ def loopFetchAndSetShanghaiCalendarId():
             pass
 
 
-def getMondayInThisWeek(_t):
+def getMondayInLastWeek(_t):
     """
     给一个"月/日/年 时:分"格式的日期字符串,返回这个日期的这个星期的星期一的日期
     :param t: '8/10/2019 10:20'
     :return: 星期一的日期   '日/月/年 0:00:00'
     """
-    dt = datetime.datetime.strptime(_t, '%m/%d/%Y %H:%M')
-    print('dt: ', dt)
+    dt = datetime.datetime.strptime(_t, '%d/%m/%Y %H:%M')
     diff = dt.weekday()
-    print('diff: ', diff)
-    for _ in range(diff):
+    for _ in range(diff + 7):
         dt = dt - datetime.timedelta(days=1)
-        print('dt: ', dt)
     return f'{dt.day}/{dt.month}/{dt.year} 0:00:00'
 
 
@@ -221,20 +258,80 @@ def baidu_ocr(img_bytes):
     return False
 
 
+def fetchStartTimes(client, targetDay):
+    """
+    返回目标日期的所有可预约时间段
+    :param client:
+    :param targetDay: 日/月/年
+    :return:
+    """
+    urlFirst = f'{BASE_URL}?fromSpecificInfo=True'  # 第一次进入先获取一次没有的话就用下面的获取下一周/上一周(改Monday可以跳)
+    urlOther = f'{BASE_URL}HomeWeb/Scheduler'
+    targetStartTimePattern = re.compile(r'input .*?%s.*?value="(.*?)"' % targetDay)
+    playloadFirst = {
+        'Language': 'zh',
+        'Office': client['CalendarType'],
+        'CalendarId': client['CalendarId'],
+        'PersonCount': '1',
+        'Command': '下一步',
+    }
+    playloadOther = {
+        'Language': 'zh',
+        'Office': client['CalendarType'],
+        'CalendarId': client['CalendarId'],
+        'PersonCount': '1',
+        'Monday': getMondayInLastWeek(f'{targetDay} 10:30'),
+        'Command': '下一周',
+    }
+    # print(getMondayInLastWeek(f'{targetDay} 10:30'))
+    """
+    r = requests.post(urlFirst, data=playloadFirst, headers=HEADERS)
+    if r.status_code == 200:
+        text = r.text
+        if targetDay in text:
+            searchResult = re.findall(targetStartTimePattern, text)
+            if searchResult:
+                # FIXME: 获取所有的StartTime
+                # StartTime = searchResult.group(1)
+                print(searchResult)
+        else:
+            r = requests.post(urlOther, data=playloadOther, headers=HEADERS)
+            if r.status_code == 200:
+                text = r.text
+                searchResult = re.findall(targetStartTimePattern, text)
+                if searchResult:
+                    # FIXME: 获取所有的StartTime
+                    # StartTime = searchResult.group(1)
+                    print(searchResult)
+        """
+    while True:
+        try:
+            r = requests.post(urlOther, data=playloadOther, headers=HEADERS)
+            if r.status_code == 200:
+                text = r.text
+                findResult = re.findall(targetStartTimePattern, text)
+                if findResult:
+                    return findResult
+        except Exception as e:
+            print(e)
+            pass
+
+
 def fetchToken(client, StartTime):
     url = f'{BASE_URL}HomeWeb/Scheduler'
     playload = {
-        'zh': 'Language',
+        'Language': 'zh',
         'Office': client['CalendarType'],
         'CalendarId': client['CalendarId'],
         'PersonCount': '1',
         # 'Monday': '',  # 测试过,这个字段不需要的
-        'Start': f'{StartTime}:00',
+        # 'Start': f'{StartTime}:00',
+        'Start': StartTime,
         'Command': '下一步',
     }
     while True:
         try:
-            r = requests.post(url, data=playload)
+            r = requests.post(url, data=playload, headers=HEADERS)
             if r.status_code == 200:
                 text = r.text
                 with open('token.html', 'w', encoding='utf-8') as fp:
@@ -257,7 +354,7 @@ def downloadCaptcha(Token):
     url = f'{BASE_URL}/Captcha?token={Token}'
     while True:
         try:
-            r = requests.get(url, stream=True)
+            r = requests.get(url, stream=True, headers=HEADERS)
             if r.status_code == 200:
                 content = r.content
                 # 新处理方式,试试?!
@@ -268,10 +365,10 @@ def downloadCaptcha(Token):
                 gray_img = naive_remove_noise(image, 4)
                 # gray_img = naive_remove_noise(content, 4)
                 gray_content = cv2.imencode('.png', gray_img)[1]
-                with open('captcha1.png', 'wb') as fp:
-                    fp.write(content)
-                with open('captcha2.png', 'wb') as fp:
-                    fp.write(gray_content)
+                # with open('captcha1.png', 'wb') as fp:
+                #     fp.write(content)
+                # with open('captcha2.png', 'wb') as fp:
+                #     fp.write(gray_content)
                 return {'url': url, 'contentOri': content, 'contentNew': gray_content}
         except Exception as e:
             print(e)
@@ -283,10 +380,10 @@ def verificationCaptcha(captchaDict, captchaType):
     try:
         CaptchaText = baidu_ocr(captchaImg)
         print('CaptchaText: ', CaptchaText)
+        return CaptchaText
     except Exception as e:
         print(e)
         return False
-    return CaptchaText
 
 
 def submit(client, StartTime, Token, CaptchaText):
@@ -297,7 +394,8 @@ def submit(client, StartTime, Token, CaptchaText):
         'CalendarId': client['CalendarId'],
         'Token': Token,
         'PersonCount': '1',
-        'StartTime': f'{StartTime}:00',
+        # 'StartTime': f'{StartTime}:00',
+        'StartTime': f'{StartTime}',
         'Lastname': client['Lastname'],
         'Firstname': client['Firstname'],
         'DateOfBirth': client['DateOfBirth'],
@@ -323,7 +421,7 @@ def submit(client, StartTime, Token, CaptchaText):
     }
     while True:
         try:
-            r = requests.post(url, data=playload)
+            r = requests.post(url, data=playload, headers=HEADERS)
             if r.status_code == 200:
                 text = r.text
                 with open('result.html', 'w', encoding='utf-8') as fp:
@@ -335,7 +433,7 @@ def submit(client, StartTime, Token, CaptchaText):
 
 
 def coroutineTask(client, StartTime):
-    print('协程开始')
+    print(f'    -[协程开始 {StartTime}]')
     while True:
         Token = fetchToken(client, StartTime)
         # print(Token)
@@ -346,19 +444,38 @@ def coroutineTask(client, StartTime):
                 break
         rText = submit(client, StartTime, Token, CaptchaText)
         if '预约成功' in rText:
-            print(f'预约成功! [护照号码: {client["TraveldocumentNumber"]} 时间: {StartTime}]')
+            print(f'☺预约成功! [护照号码: {client["TraveldocumentNumber"]} 时间: {StartTime}]')
             return True
         elif '此时间已有其他预约' in rText:
             print(f'预约失败,此时间已有其他预约! [护照号码: {client["TraveldocumentNumber"]} 时间: {StartTime}]')
+            return False
+        elif '您已经有一个预约' in rText:
+            print(f'恭喜!该客户已经成功预约了一个时间!不需要重复预约了! [护照号码: {client["TraveldocumentNumber"]}]')
             return False
         elif '您所输入的与图像中的文字不符' in rText:
             print('您所输入的与图像中的文字不符 重新抓取')
 
 
 def threadTask(client):
-    print('线程开始')
-    greenlets = [gevent.spawn(coroutineTask, client, StartTime) for StartTime in client['StartTime'].split(', ')]
+    print(f'-[线程开始 {client["StartTime"]}]')
+    targetDays = client['StartTime'].split(' ')
+    startTimes = []
+    for targetDay in targetDays:
+        tmpStartTimes = fetchStartTimes(client, targetDay)
+        startTimes += tmpStartTimes
+    # greenlets = [gevent.spawn(coroutineTask, client, StartTime) for StartTime in client['StartTime'].split(', ')]
+    greenlets = [gevent.spawn(coroutineTask, client, StartTime) for StartTime in startTimes]
     gevent.joinall(greenlets)
+    print('线程结束')
+
+
+# class Thread(QThread):
+#     def __init__(self, client):
+#         self.client = client
+#         super().__init__()
+#
+#     def run(self):
+#         threadTask(self.client)
 
 
 def demo():
@@ -376,10 +493,13 @@ def demo():
                     break
             rText = submit(client, StartTime, Token, CaptchaText)
             if '预约成功' in rText:
-                print(f'预约成功! [护照号码: {client["TraveldocumentNumber"]} 时间: {StartTime}]')
+                print(f'☺预约成功! [护照号码: {client["TraveldocumentNumber"]} 时间: {StartTime}]')
                 return True
             elif '此时间已有其他预约' in rText:
                 print(f'预约失败,此时间已有其他预约! [护照号码: {client["TraveldocumentNumber"]} 时间: {StartTime}]')
+                return False
+            elif '您已经有一个预约' in rText:
+                print(f'恭喜!该客户已经成功预约了一个时间!不需要重复预约了! [护照号码: {client["TraveldocumentNumber"]}]')
                 return False
             elif '您所输入的与图像中的文字不符' in rText:
                 print('您所输入的与图像中的文字不符 重新抓取')
@@ -399,10 +519,9 @@ if __name__ == "__main__":
         if not G_DEBUG:
             with ThreadPoolExecutor(
                     max_workers=len(G_PEKING_CLIENTS) + len(G_SHANGHAI_CLIENTS)) as executor:
-                print(len(G_PEKING_CLIENTS) + len(G_SHANGHAI_CLIENTS))
                 for client in G_PEKING_CLIENTS:
                     executor.submit(threadTask, (client))
-                loopFetchAndSetShanghaiCalendarId()  # 死循环直至上海(正式)面签机会的CalendarId出现
+                loopFetchAndSetUnknownCalendarId('SHANGHAI')  # 死循环直至上海(正式)面签机会的CalendarId出现
                 for client in G_SHANGHAI_CLIENTS:
                     executor.submit(threadTask, (client))
         else:
@@ -411,9 +530,18 @@ if __name__ == "__main__":
                 print(len(G_PEKING_CLIENTS_DEBUG) + len(G_SHANGHAI_CLIENTS_DEBUG))
                 for client in G_PEKING_CLIENTS_DEBUG:
                     executor.submit(threadTask, (client))
-                # loopFetchAndSetShanghaiCalendarId()  # 死循环直至上海(正式)面签机会的CalendarId出现
+                # loopFetchAndSetUnknownCalendarId(SHANGHAI)  # 死循环直至上海(正式)面签机会的CalendarId出现
                 for client in G_SHANGHAI_CLIENTS_DEBUG:
                     executor.submit(threadTask, (client))
+
     # """
     # demo()
+    """
+    fetchBaiduOCRClients()
+    fetchClients()
+    startTime = G_SHANGHAI_CLIENTS_DEBUG[0]['StartTime'].split(' ')
+    for s in startTime:
+        startTimes = fetchStartTimes(G_SHANGHAI_CLIENTS_DEBUG[0], s)
+        print(startTimes)
+    """
     pass
